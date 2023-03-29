07d9afb4ec7f8dd1c3f4bb4c93e1c9878563c869
Stephen Chau <chtzch0713@gmail.com>
2023-03-29 16:34:41 -0700

    basic structure for project

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname + "/client"));
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
